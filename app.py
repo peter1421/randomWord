810a34614313057ef7d905cbd6401fb7b500f5ab
peter1421 <51041311+peter1421@users.noreply.github.com>
2022-04-15 14:43:27 +0800

    清除多餘file

--- app.py
+++ app.py
@@ -56,30 +56,6 @@ def showDataNoun(wordType):
     return render_template("table.html", title=wordType, labels=labels, data=data)
 
 
-# @app.route("/add_data")
-# def index_add_data():
-#     try:
-#         data = [request.args.get("adj", ""), request.args.get("n", ""), request.args.get(
-#             "p", ""), request.args.get("v", "")]
-#         op = 1
-#         print("正在輸入", data)
-#         for x in range(3):
-#             if(data[x] == ''):
-#                 op = 0
-#                 print("NULL")
-#                 break
-#         if(op == 1):
-#             print("資料正在存入資料庫")
-#             t = D.DataBase()
-#             t.insert(data[0], data[1], data[2], data[3])
-#             a = D.DataBase()
-#             a.Ndelete()
-#             return render_template("add_data.html", data=data)
-#     except:
-#         data = "fuck"
-#     return render_template("erro.html")
-
-
 @app.route("/show_data")
 def index_show_data():
     t = D.DataBase()
@@ -95,22 +71,3 @@ def index_show_data():
 if __name__ == "__main__":
     app.run(port=30331, debug=True)
 
-
-# @app.route("/cal")
-# def index_c():
-#     max = int(request.args.get("max", ""))
-#     result = 0
-#     for x in range(1, max+1):
-#         result += x
-#     return render_template("result.html", data=result)
-
-
-# @app.route("/show")
-# def index_s():
-#     name = request.args.get("n", "")
-#     return "歡迎"+name
-
-
-# @app.route("/page")
-# def index_p():
-#     return render_template("page.html")
